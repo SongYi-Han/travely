d887d0adde31d7c7ff558a0effc739a94c243112
Songyi Han <songyi.lee.han@gmail.com>
2018-11-30 10:51:58 -0500

    add config prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.compile = true
+      config.assets.digest = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
